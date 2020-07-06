14b58eeb554c3d8899901d507bc8b5d8f485e7cc
kalebgarrison <kalebgarrison@gmail.com>
2020-07-06 19:31:53 -0400

    Added two views and a notes api

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// View routes
+// Use GET method here
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+// API routes
+app.get("/api/notes", (req, res) => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
+        if (err){
+            return res.send("An error occurred reading your data.");
+        }
+        res.json(data);
+    })
+})
 
 
 
@@ -11,13 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-    res.sendFile(path.join(__dirname, "public/index.html"));
-})
-
-
 
 app.listen(PORT, (req, res) => {
-    console.log(`Currently running on http://localhost:${PORT}`);
+  console.log(`Currently running on http://localhost:${PORT}`);
 });
