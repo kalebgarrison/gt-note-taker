653adcc54f61bb521b3700933180965f36bc8cbe
kalebgarrison <kalebgarrison@gmail.com>
2020-07-06 20:33:00 -0400

    Added post route to api

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // View routes
 // Use GET method here
 app.get("/", (req, res) => {
@@ -17,22 +20,32 @@ app.get("/notes", (req, res) => {
 });
 // API routes
 app.get("/api/notes", (req, res) => {
-    fs.readFile("db/db.json", "utf8", (err, data) => {
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) {
+      return res.send("An error occurred reading your data.");
+    }
+    const arrayOfNotes = JSON.parse(data);
+    res.json(arrayOfNotes);
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf8", (err, data) => {
+    if (err) {
+      return res.send("An error occurred reading your data.");
+    }
+    const arrayOfNotes = JSON.parse(data);
+    arrayOfNotes.push(req.body);
+    fs.writeFile("db/db.json", JSON.stringify(arrayOfNotes), "utf8", (err) => {
         if (err){
-            return res.send("An error occurred reading your data.");
+            return res.send("An error occurred writing your data.");
         }
-        res.json(data);
+        res.json(arrayOfNotes);
     })
-})
-
-
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+  }); 
+});
 
 
 app.listen(PORT, (req, res) => {
   console.log(`Currently running on http://localhost:${PORT}`);
-});
+}); 
